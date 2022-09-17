f010de3abf85eeb56699bbf989533dc0cda4107a
Leningsantiago <jorsanbareban@gmail.com>
2022-09-17 03:35:36 -0500

    bd

--- bd/bd_cafeteria.sql
+++ bd/bd_cafeteria.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 17-09-2022 a las 00:19:38
--- Versión del servidor: 10.4.20-MariaDB
--- Versión de PHP: 8.0.8
+-- Servidor: 127.0.0.1:3305
+-- Tiempo de generación: 17-09-2022 a las 10:33:53
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---bd_cafeteriabd_cafeteria
+--
 -- Base de datos: `bd_cafeteria`
 --
 
@@ -37,6 +37,7 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`category_id`, `name_category`) VALUES
+(12, 'BEBIDAS CALIENTES'),
 (3, 'CAPPUCCINOS'),
 (2, 'ESPRESSOS'),
 (1, 'TINTOS'),
@@ -64,7 +65,42 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `product_reference`, `product_price`, `product_weight`, `categories_fk_id`, `product_stock`, `product_create_date`) VALUES
-(46, 'asdas', 'asd', 1200, 1, 3, 60, '2022-09-16 22:18:07');
+(1, 'AMERICANO', 'cafe_001', 1300, 1, 1, 20, '2022-09-17 01:30:57'),
+(2, 'DEL CAMPO', 'cafe_002', 1800, 1, 1, 23, '2022-09-17 01:31:29'),
+(3, 'VIENESSE', 'cafe_003', 2000, 1, 1, 9, '2022-09-17 01:31:53'),
+(4, 'NUESTRA TIERRA', 'cafe_004', 2500, 1, 1, 11, '2022-09-17 01:32:56'),
+(5, 'CORTO', 'cafe_005', 3000, 1, 2, 2, '2022-09-17 01:33:25'),
+(6, 'DOBLE', 'cafe_006', 5000, 1, 2, 17, '2022-09-17 01:33:54'),
+(7, 'CHOCOLATE', 'cafe_007', 2500, 1, 12, 40, '2022-09-17 01:35:04'),
+(8, 'TÉ EN LECHE', 'cafe_008', 2000, 1, 12, 25, '2022-09-17 01:35:39'),
+(9, 'TÉ EN AGUA', 'cafe_009', 1500, 1, 12, 60, '2022-09-17 01:36:13'),
+(10, 'SENCILLO', 'cafe_010', 6000, 2, 3, 0, '2022-09-17 01:37:10');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sales`
+--
+
+CREATE TABLE `sales` (
+  `sales_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `sales_amount` int(11) NOT NULL,
+  `sales_create_date` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sales`
+--
+
+INSERT INTO `sales` (`sales_id`, `product_id`, `price`, `sales_amount`, `sales_create_date`) VALUES
+(1, 5, 3000, 1, '2022-09-17 06:58:02'),
+(2, 1, 1300, 10, '2022-09-17 07:16:52'),
+(3, 3, 2000, 21, '2022-09-17 07:17:49'),
+(4, 1, 1300, 10, '2022-09-17 07:18:35'),
+(5, 5, 3000, 2, '2022-09-17 07:26:34'),
+(6, 1, 1300, 10, '2022-09-17 07:31:24');
 
 --
 -- Índices para tablas volcadas
@@ -85,6 +121,12 @@ ALTER TABLE `products`
   ADD KEY `categories_fk_id` (`categories_fk_id`);
 
 --
+-- Indices de la tabla `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`sales_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -92,13 +134,19 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT de la tabla `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT de la tabla `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `sales_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restricciones para tablas volcadas
@@ -114,4 +162,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-bd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteriabd_cafeteria
