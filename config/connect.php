e454cedf3c9cc81978b08e7c001b87828a3d537d
Leningsantiago <63259718+leningsantiago@users.noreply.github.com>
2022-09-17 03:38:44 -0500

    terminado

--- config/connect.php
+++ config/connect.php
@@ -1,36 +1,8 @@
 <?php
 
 // connect database
-
-// class Database {
-//     private $hostname = "localhost";
-//     private $database = "bd_cafeteria";
-//     private $username = "root";
-//     private $password = "";
-//     private $charset = "utf8";
-
-
-//     function connect(){
-//         try {
-//             $conexion = "mysql:host=".$this->hostname.";dbname=".$this->database.";
-//             charset=".$this->charset;
-//             $options = [
-//                 PDO::ATTR_ERRMODE =>PDO::ERRMODE_EXCEPTION,
-//                 PDO::ATTR_EMULATE_PREPARES => false,
-//             ];
-
-//             $pdo = new PDO($conexion, $this->username, $this->password, $options);
-
-//             return $pdo;
-//         } catch (PDOException $e) {
-//             echo 'Error conexion '. $e->getMessage();
-//             exit;
-//         }
-//     }
-// }
-
 //  ======== conexion a nuestra base de datos ==========
-$serverName = "127.0.0.1:3305";
+$serverName = "127.0.0.1";//localhost
 $userName = "root"; //root
 $password = "";
 
